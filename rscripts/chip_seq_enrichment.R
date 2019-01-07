e969bb289c76ea9cc39c4ee9b3c8bba07967052c
dechaot <dechaotian@gmail.com>
2019-01-07 15:30:55 -0500

    update enrichment test

---
+++ rscripts/chip_seq_enrichment.R
@@ -0,0 +1,53 @@
+rm(list=ls())
+source('src/chip_seq_enrichment_test.R')
+cells = c('gm12878', 'k562')
+ws = c('5000', '10000', '50000', '100000')
+type = c('individual', 'population')
+chip_coverage_cutoffs=c(0.05, 0.1, 0.2)
+result = c()
+for(cell in cells){
+    hims_gs = gs_from_hims('data/him_summary_allinone.txt', cell=cell)
+    for(w in ws){
+        for(ty in type){
+            for(cc in chip_coverage_cutoffs){
+                tmp = chipenrich_him_main(him_gs=him_gs, cell=cell, w=w, method.test=ty, tf_group=F, chip_coverage = cc)
+                tmp = data.frame(cell, w, ty,cc, tmp, stringsAsFactors = F)
+                result = rbind(result, tmp)
+            }
+        }
+    }
+}
+result$w = factor(result$w, levels=ws)
+result$cc = factor(result$cc, levels=chip_coverage_cutoffs)
+result_pop = subset(result, ty=='population')
+result_pop_long = melt(result_pop, measure.vars = c('prop', 'pval.1', 'background'))
+ggplot(result_pop_long, aes(y=Prop.background, x=as.factor(value), color=variable)) + geom_violin() +
+    facet_grid(cell~w)
+result_pop_sum = aggregate(cbind(prop, pval.1, background)~cell+ w , data=result_pop,
+                        FUN=function(z) sum(z) / length(z))
+result_pop_sum = result_pop_sum[order(result_pop_sum$cell, result_pop_sum$w),]
+result_pop_sum
+# aggregate TFs in invidual type, x-axis is prop.background
+result_indi = aggregate(cbind(prop, pval.1, background) ~ cell+ w + tf + Prop.background, data=result,
+                        FUN=function(z) sum(z) / length(z),
+                        subset=result$ty == 'individual')
+result_indi_long = melt(result_indi, measure.vars=c('prop', 'pval.1', 'background'))
+ggplot(result_indi_long, aes(x=Prop.background, y=value, color=variable)) + geom_point() +
+    facet_grid(cell~w)
+result_indi_sum = aggregate(cbind(prop, pval.1, background) ~ cell+ w, data=result_indi,
+                        FUN=function(z) sum(z>=0.5) / length(z))
+result_indi_sum = result_indi_sum[order(result_indi_sum$cell, result_indi_sum$w), ]
+result_indi_sum
+# do not aggregate; plot him_ids and TFs
+result_indi_him = subset(result, ty=='individual')
+ggplot(result_indi_him, aes(x=as.factor(tf), y=Prop)) + geom_point(size=0.5)+
+    facet_grid(cell~w)
+
+# observation 1. window size does not matter much for pval & backgroud, but matters a lot on absolute proportion.
+               
+# thought, reconsider the definition of enrichment
+# pval is not suitable for individual hims due to small gene number in hims; this is done
+# try different statistical test; hypergeo, binomial, fisher exact; not necessary
+# number of targets per him; it could be a confounding variable 
+# output the number for each TF to test whether there are significant variation among TFs, and the factors contribute to the variations 
+# change the value of majority (0.5)
--- src/chip_seq_enrichment_test.R
+++ src/chip_seq_enrichment_test.R
@@ -1,6 +1,6 @@
-rm(list=ls())
 library(splitstackshape)
-#library(chipenrich)
+library(ggplot2)
+library(reshape2)
 gs_from_hims = function(i, cell){
     # load genes in the GRN of the cell type
     genes = read.table(paste('data/gene_symbol_list_', cell,'.txt', sep=''), stringsAsFactors = F)
@@ -54,8 +54,8 @@ chipenrich_him = function(df1, df2, method.test='individual'){
             res = rbind(res, tmp)
         }
         res = cbind(res, p_back)
-        rownames(res) = him_u
     } else if(method.test=='population'){
+        him_u = 'Population'
         genes = df1[, 'genes']
         n = length(genes)
         gene_bool = rownames(df2) %in% genes
@@ -64,10 +64,9 @@ chipenrich_him = function(df1, df2, method.test='individual'){
         pval = binom.test(x=n1, n=n, p=p_back, alternative = 'g')$p.value
         res = c(n, n1, pval, p_him, p_back)
         res = t(as.data.frame(res))
-        rownames(res) = 'Population'
     }
     colnames(res) = c('N.gene', 'N.gene.w.peak', 'pval', 'Prop', 'Prop.background')
-    #res = data.frame(him=him_u, res, stringsAsFactors = F)
+    res = data.frame(him=him_u, res, stringsAsFactors = F)
     return(res)
 }
 
@@ -80,69 +79,26 @@ chipenrich_him_combine = function(tfs, hims_gs, peak_gene, method.test='individu
     }
     return(res)
 }
-chipenrich_result_summarize = function(df, method, method.test='individual'){
-    if(method=='prop'){
-        ind = df$Prop >= 0.8
-    } else if (method=='pval'){
-        ind = df$pval <= 0.05
-    } else if (method=='background'){
-        ind = df$Prop > df$Prop.background 
-    } else {
-        stop("Double chcek the method")
-    }
-    if(method.test == 'individual'){
-        tmp0 = as.data.frame(table(df[, 'tf'])); tmp0[, 1] = as.character(tmp0[, 1])
-        tmp1 = as.data.frame(table(df[ind, 'tf'])); tmp1[, 1] = as.character(tmp1[, 1])
-        rownames(tmp0) = tmp0[, 1]; rownames(tmp1) = tmp1[, 1]
-        tmp2 = cbind(tmp0[rownames(tmp1), ], tmp1)
-        ind = tmp2[, 4] / tmp2[, 2] >= 0.5
-    } 
-    
-    ind = as.integer(ind)
-    tmp2.sum = as.data.frame(table(ind))
-    tmp2.sum[,1] = as.character(tmp2.sum[, 1])
-    tmp3 = c(0, 0); names(tmp3) = as.character(0:1)
-    tmp3[tmp2.sum[, 1]] = tmp2.sum[, 2]
-    return(tmp3)
+chipenrich_result_summarize = function(df, method.test='individual', tf_group=F){
+    sum.method = c('prop', 'pval', 'background')
+    ind = data.frame(prop=df$Prop >= 0.8, pval=df$pval <= 0.05, background=df$Prop > df$Prop.background)
+    ind = apply(ind, 2, as.integer)
+    df = data.frame(df, ind, stringsAsFactors = F)
+    return(df)
 }
-chipenrich_him_main = function(cell, w, method.test){
-    hims_gs = gs_from_hims('data/him_summary_allinone.txt', cell=cell)
+chipenrich_him_main = function(him_gs, cell, w, method.test, tf_group=F, chip_coverage=0.05){
     peak_gene = load_peak_table(cell=cell, w=w)
+    ## filter out TFs with <= 5% genes having their peaks; these TFs might not co-operate with other TFs to regulate multiple genes
+    peak_coverage = apply(peak_gene, 2, function(z) sum(z>0) / length(z))
+    peak_gene = peak_gene[, peak_coverage>= chip_coverage]
     tfs_wc = colnames(peak_gene)
     res = chipenrich_him_combine(tfs=tfs_wc, hims_gs=hims_gs, peak_gene = peak_gene, method.test=method.test)
-    sum.method = c('prop', 'pval', 'background')
-    res.sum = sapply(sum.method, function(z) chipenrich_result_summarize(df=res, method=z, method.test=method.test) )
+    res.sum = chipenrich_result_summarize(df=res, method.test=method.test, tf_group=tf_group)
     return(res.sum)
 }
-#cell='gm12878';w='10000'
-#chipenrich_him_main(cell=cell, w=w, method.test='individual')
-#chipenrich_him_main(cell=cell, w=w, method.test='population')
-cells = c('gm12878', 'hela', 'k562')
-ws = c('5000', '10000', '50000', '100000')
-result = list()
-for(cell in cells){
-    result[[cell]] = list()
-    for(w in ws){
-        print(c(cell, w))
-        result[[cell]][[w]] = chipenrich_him_main(cell=cell, w=w, method.test='population')
-    }
-}
-result
-result_individual = list()
-for(cell in cells){
-    result_individual[[cell]] = list()
-    for(w in ws){
-        print(c(cell, w))
-        result_individual[[cell]][[w]] = chipenrich_him_main(cell=cell, w=w, method.test='population')
-    }
-}
-result_individual
-# observation 1. window size does not matter much for pval & backgroud, but matters a lot on absolute proportion.
-# thought, reconsider the definition of enrichment
-# try different statistical test; hypergeo, binomial, fisher exact
-# number of targets per him; it could be a confounding variable 
 #### method  overhaul stops here 
 
+#library(chipenrich)
 # it is a model-based method to test enrichment of TF Chip-seq peaks in a given gene sets
 # it is a competive model which compare the gene set and the other genes
 #gs_from_hims = function(i, cell){
@@ -183,72 +139,72 @@ result_individual
 #    return(result)
 #}
 
-TFs_w_chip_seq = function(cell){
-    meta = read.table(paste('data/', cell, '/metadata_slim_from_all.tsv', sep=''), header=F, stringsAsFactors = F, sep='\t')
-    meta = meta[, c(1, 13)]
-    meta[, 'filepath'] = paste('data/', cell, '/', meta[,2], '_', meta[,1], '.bed.gz', sep='')
-    meta = meta[, c(2, 3)]
-    colnames(meta) = c('TF', 'filepath')
-    meta$TF = gsub('-human','', meta$TF)
-    rownames(meta) = meta$TF
-    return(meta)
-}
+#TFs_w_chip_seq = function(cell){
+#    meta = read.table(paste('data/', cell, '/metadata_slim_from_all.tsv', sep=''), header=F, stringsAsFactors = F, sep='\t')
+#    meta = meta[, c(1, 13)]
+#    meta[, 'filepath'] = paste('data/', cell, '/', meta[,2], '_', meta[,1], '.bed.gz', sep='')
+#    meta = meta[, c(2, 3)]
+#    colnames(meta) = c('TF', 'filepath')
+#    meta$TF = gsub('-human','', meta$TF)
+#    rownames(meta) = meta$TF
+#    return(meta)
+#}
 # copy bed files from the server in case they do not exist in the local directory
 #scp -r cmu:/hive/dechaot/Data/ENCODE/chip-seq/gm12878 data/
-chip_enrichment = function(cell){
-    meta_tf = TFs_w_chip_seq(cell=cell)
-    hims_gs = gs_from_hims('data/him_summary_allinone.txt', cell=cell)
-    l1 = names(hims_gs[['comb']]); l2 = meta_tf$TF
-    # some TFs with Chip-seq data are not in the GRNs such as JUND
-    meta_tf = meta_tf[l2 %in% l1, ]
-    meta_tf['NFE2', ]
-    pval = list()
-    i = which(meta_tf$TF == 'NFE2')
-    for(i in 1:nrow(meta_tf)){
-        tf = meta_tf[i, 1]
-        gs_path = paste('inter_results/gs_', tf,'.txt', sep='')
-        write.table(hims_gs[['comb']][[tf]], file=gs_path, col.names = T, row.names = F, sep='\t', quote=F)
-        peaks_tf = read.table(gzfile(meta_tf[i, 2]), header=F, sep='\t', stringsAsFactors = T)
-        peaks_tf = peaks_tf[, 1:3]
-        colnames(peaks_tf) = c('chrom', 'start', 'end')
-        tryCatch({
-            # the number of GO terms in the genesets matters
-            results = chipenrich(peaks = peaks_tf, genome = 'hg19', genesets = gs_path,
-                          locusdef = '10kb', num_peak_threshold=1, qc_plots = FALSE, out_name = NULL, n_cores=1)
-            results.be = results$results
-            pval[[tf]] = results.be[, c('Geneset.ID', 'P.value', 'Odds.Ratio', 'Status', 'N.Geneset.Genes', 'N.Geneset.Peak.Genes', 'Geneset.Avg.Gene.Length')]
-        }, error=function(e) {conditionMessage(e)})
-    }
-    pval = do.call(rbind.data.frame, pval) 
-    ind = meta_tf$TF %in% pval[, 1]
-    ngene = sapply(hims_gs[['comb']][meta_tf$TF], nrow)
-    print(table(ind))
-    print(fivenum(ngene[ind]))
-    print(fivenum(ngene[!ind]))
-    return(pval)
-}
-
-pval = chip_enrichment(cell='gm12878')
-table(pval[, 'Status'], pval[, 'P.value']<=0.05)
-pval = chip_enrichment(cell='k562')
-table(pval[, 'Status'], pval[, 'P.value']<=0.05)
-pval = chip_enrichment(cell='hela')
-table(pval[, 'Status'], pval[, 'P.value']<=0.05)
-# errors for some TFs & characterize the list
-    # could be 0 peaks
-# refine the peaks by motif enrichment 
-# window size 
-# m
-RUNX3 NFATC1 ZBTB33  ESRRA   ELF1 
-20     53    193    334    640 
-PAX8 NR2F1  NFIC  IRF5  NFE2 
-14.0  82.5 161.0 345.0 869.0 # k562
-ZBED1  E4F1  NFIC   MYC  NFE2 
-42   137   249   467  1638 
-E2F8 HMBOX1  NR2F1   ARNT    SP1 
-80    225    326    516   1911 
-# hela
-CTCF   CTCF  GABPA   REST NFE2L2 
-276    301    403    822   1164 
-EP300 EP300 EP300  MAFK  MAFK 
-230   230   513   796   796 
+#chip_enrichment = function(cell){
+#    meta_tf = TFs_w_chip_seq(cell=cell)
+#    hims_gs = gs_from_hims('data/him_summary_allinone.txt', cell=cell)
+#    l1 = names(hims_gs[['comb']]); l2 = meta_tf$TF
+#    # some TFs with Chip-seq data are not in the GRNs such as JUND
+#    meta_tf = meta_tf[l2 %in% l1, ]
+#    meta_tf['NFE2', ]
+#    pval = list()
+#    i = which(meta_tf$TF == 'NFE2')
+#    for(i in 1:nrow(meta_tf)){
+#        tf = meta_tf[i, 1]
+#        gs_path = paste('inter_results/gs_', tf,'.txt', sep='')
+#        write.table(hims_gs[['comb']][[tf]], file=gs_path, col.names = T, row.names = F, sep='\t', quote=F)
+#        peaks_tf = read.table(gzfile(meta_tf[i, 2]), header=F, sep='\t', stringsAsFactors = T)
+#        peaks_tf = peaks_tf[, 1:3]
+#        colnames(peaks_tf) = c('chrom', 'start', 'end')
+#        tryCatch({
+#            # the number of GO terms in the genesets matters
+#            results = chipenrich(peaks = peaks_tf, genome = 'hg19', genesets = gs_path,
+#                          locusdef = '10kb', num_peak_threshold=1, qc_plots = FALSE, out_name = NULL, n_cores=1)
+#            results.be = results$results
+#            pval[[tf]] = results.be[, c('Geneset.ID', 'P.value', 'Odds.Ratio', 'Status', 'N.Geneset.Genes', 'N.Geneset.Peak.Genes', 'Geneset.Avg.Gene.Length')]
+#        }, error=function(e) {conditionMessage(e)})
+#    }
+#    pval = do.call(rbind.data.frame, pval) 
+#    ind = meta_tf$TF %in% pval[, 1]
+#    ngene = sapply(hims_gs[['comb']][meta_tf$TF], nrow)
+#    print(table(ind))
+#    print(fivenum(ngene[ind]))
+#    print(fivenum(ngene[!ind]))
+#    return(pval)
+#}
+#
+#pval = chip_enrichment(cell='gm12878')
+#table(pval[, 'Status'], pval[, 'P.value']<=0.05)
+#pval = chip_enrichment(cell='k562')
+#table(pval[, 'Status'], pval[, 'P.value']<=0.05)
+#pval = chip_enrichment(cell='hela')
+#table(pval[, 'Status'], pval[, 'P.value']<=0.05)
+## errors for some TFs & characterize the list
+#    # could be 0 peaks
+## refine the peaks by motif enrichment 
+## window size 
+## m
+#RUNX3 NFATC1 ZBTB33  ESRRA   ELF1 
+#20     53    193    334    640 
+#PAX8 NR2F1  NFIC  IRF5  NFE2 
+#14.0  82.5 161.0 345.0 869.0 # k562
+#ZBED1  E4F1  NFIC   MYC  NFE2 
+#42   137   249   467  1638 
+#E2F8 HMBOX1  NR2F1   ARNT    SP1 
+#80    225    326    516   1911 
+## hela
+#CTCF   CTCF  GABPA   REST NFE2L2 
+#276    301    403    822   1164 
+#EP300 EP300 EP300  MAFK  MAFK 
+#230   230   513   796   796 
