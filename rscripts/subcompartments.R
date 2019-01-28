a9c46c6e2f380d0873723bc39114600580b982d5
dechao tian <dechaotian@gmail.com>
2019-01-28 03:49:17 -0500

    misc

--- rscripts/subcompartments.R
+++ rscripts/subcompartments.R
@@ -1,4 +1,5 @@
 rm(list=ls())
+source('src/boxdot.R')
 load_subcompartment  = function(){
     sub = read.table('data/subcompartment_inter_genes.txt', header=F, stringsAsFactors = F)
     sub = sub[ order(sub[, 14], -sub[, 15]), ]
@@ -60,6 +61,23 @@ compute_freq = function(x, y){
     res = data.frame(names = cats, mat, stringsAsFactors = F)
     return(res)
 }
+extract_sub_freq = function(x, cutoffs, labels){
+#    Apercent = c()
+#    cells = unique(X$cell)
+#    for( ic in 1:length(cells)){
+#        x = df[df$cell == cells[ic], 'A_percent']
+        y=cut(x, breaks=cutoffs, labels=labels, include.lowest = T, right=F)
+        z = as.data.frame(table(y))
+        z[, 2] = z[, 2] / sum(z[, 2]) * 100
+#        z = data.frame(z, cells[ic], stringsAsFactors = F)
+        w = z[, 2]
+        names(w) = z[, 1]
+#        Apercent = rbind(Apercent, z)
+#    }
+#    colnames(Apercent) = c('category', 'proportion')
+#    return(Apercent)
+    return(w)
+}
 ## main starts here
 sub = load_subcompartment()
 # proportion of subcompartments
@@ -70,12 +88,40 @@ hims = hims[hims$A_percent == 100,]
 hims_gene = lapply(hims$genes, function(z) unlist(strsplit(z, ';|,')))
 hims_tf = lapply(hims$TFs, function(z) unlist(strsplit(z, ';|,')))
 hims_sub = sapply(hims_gene, function(z) subcompartment_distribution(genes=z, gene2sub=sub, dominate =F))
+hims_sub = hims_sub * 100
 hims_sub = t(hims_sub)
 rownames(hims_sub) = hims$index
 fivenum(rowSums(hims_sub))
-sum(rowSums(hims_sub) < 0.5)
+#sum(rowSums(hims_sub) ==0)
 # 27 HIMs with all/majority their genes are not in any subcompartments
-hims_sub = hims_sub[ rowSums(hims_sub) > 0.5, ]
+# optional
+#hims_sub = hims_sub[ rowSums(hims_sub) > 0, ]
+# create categories 
+cutoffs = c(0, 50, 80,   99.99999999999,101)
+labels = c('[0, 50)', '[50, 80)', '[80, 100)', '100' )
+hims_sub_freq = apply(hims_sub[, 1:2], 2, function(z) extract_sub_freq(x=z, cutoffs = cutoffs, labels = labels))
+hims_sub_freq
+sum(hims_sub_freq[2:4, ])
+# prepare the table for figures 
+hims_sub_freq_long = c()
+hims_sub_freq_long[1:3] = hims_sub_freq[4:2, 1]
+hims_sub_freq_long[5:7] = hims_sub_freq[2:4, 2]
+hims_sub_freq_long[4] = 100 - sum(hims_sub_freq_long, na.rm=T)
+names1 = paste(labels[4:2], '\nA1', sep='')
+names2 = paste(labels[2:4], '\nA2', sep='')
+names3 = c(names1, 'Others', names2)
+names3 = factor(names3, levels=names3)
+hims_sub_freq_long = data.frame(cat=names3, freq=hims_sub_freq_long)
+# choose 3 colors 
+col_list = c('stable' = "#2b83ba", 'cs' = "#d7191c")
+pdf('sup_fig/Subcomp_percent_gm12878.pdf', width=3, height = 2.5/2)
+ggplot(hims_sub_freq_long, aes(x=cat, y=freq)) + geom_col(width=0.5, fill=col_list[1], alpha=0.5) + 
+    ylab('% of HIMs') + xlab('% of genes in subcompartments') + 
+    theme(axis.title = element_text(size=8), axis.text=element_text(size=6), 
+          axis.text.x=element_text(angle=25, hjust=1), axis.ticks.x=element_blank())
+dev.off()
+    
+# stop here
 (apply(hims_sub, 2, function(z) sum(z>=1)) / nrow(hims_sub))
 # 0.3135593 0.0819209 0.0000000 0.0000000 0.0000000 0.0000000 
 #0.184590690 0.064205457 0.001605136 0.000000000 0.000000000 0.003210273 
